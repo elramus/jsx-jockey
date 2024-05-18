dbfea57cf7bd5014218e2b848b4810a9567883ce
Luke Ramus <luke.ramus@gmail.com>
2024-05-17 22:13:15 -0500

    quotes

--- src/wrap-with-tag.ts
+++ src/wrap-with-tag.ts
@@ -23,8 +23,8 @@ export const wrapWithTag = (
 	if (nodeToWrap) {
 		const start = document.positionAt(nodeToWrap.pos)
 		const end = document.positionAt(nodeToWrap.end)
-		const openingTag = `<>`
-		const closingTag = `</>`
+		const openingTag = "<>"
+		const closingTag = "</>"
 		edits.push(vscode.TextEdit.insert(start, openingTag))
 		edits.push(vscode.TextEdit.insert(end, closingTag))
 		insertionPoints.push(nodeToWrap.pos)
